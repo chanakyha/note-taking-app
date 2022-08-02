0c6f49bb7e9b35eed7261aba0ddfc5b9eb8d079c
Chanakyha <vetrichanakyha2003@gmail.com>
2022-08-03 00:15:16 +0530

    Added Login page

---
+++ src/Login.js
@@ -0,0 +1,23 @@
+import React from "react";
+import { GrGoogle } from "react-icons/gr";
+
+const Login = () => {
+  return (
+    <div className="w-screen h-screen flex items-center justify-center">
+      <div className="card w-96 bg-neutral text-neutral-content">
+        <div className="card-body items-center text-center">
+          <h2 className="card-title">Login with Google to Continue!</h2>
+
+          <div className="card-actions justify-end">
+            <button className="btn btn-primary flex items-center space-x-2">
+              <GrGoogle />
+              <p>Login</p>
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
--- src/index.js
+++ src/index.js
@@ -3,12 +3,11 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
+import Login from "./Login";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+
+const login = false;
+root.render(<React.StrictMode>{login ? <App /> : <Login />}</React.StrictMode>);
 
 reportWebVitals();
