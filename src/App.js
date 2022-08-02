eab6768b6ea32517c4170a6d13d71444f3cd492f
Chanakyha <vetrichanakyha2003@gmail.com>
2022-08-03 01:10:23 +0530

    Added the Note text Field

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React from "react";
+import Content from "./components/Content";
 
 import Navbar from "./components/Navbar";
 
 function App() {
   return (
     <div className="w-screen h-screen bg-[#212025]">
-      <Navbar>Content</Navbar>
+      <Navbar>
+        <Content />
+      </Navbar>
     </div>
   );
 }
