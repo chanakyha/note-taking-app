f792664f9767359044baba7d6471e468e43b05a9
Chanakyha <vetrichanakyha2003@gmail.com>
2022-08-03 18:09:35 +0530

    Display Google profile pic in avatar

--- src/components/Avatar.js
+++ src/components/Avatar.js
@@ -2,11 +2,11 @@ import React from "react";
 import { useAuth } from "../lib/useAuth";
 
 const Avatar = ({ className }) => {
-  const { onSignout } = useAuth();
+  const { onSignout, user } = useAuth();
   return (
     <div onClick={onSignout} className={"avatar " + className}>
       <div className="w-12 rounded-full border-2">
-        <img src="https://placeimg.com/192/192/people" />
+        <img src={user?.photoURL} />
       </div>
     </div>
   );
